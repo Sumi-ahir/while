7cd05a7846a171d4f5292b1cb59c163b2412a68d
sumita <sumi.sparkal@gmail.com>
2026-01-02 05:51:52 +0530

    reverse number

---
+++ reverse.js
@@ -0,0 +1,8 @@
+let prompt=require('prompt-sync')();
+let n=Number(prompt("Enter a number :"));
+let rem,rev=0;
+while(n>0){
+    rev=(rev*10)+(n%10);
+     n=Math.floor(n/10);
+    
+}console.log(rev);
